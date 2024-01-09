32ca1cf10913c36a7d4e949645ec46d2679bbbca
ruisucode <theboss7lol@gmail.com>
2024-01-09 00:19:08 -0400

    refactor:build
    * Se hicieron cambios para producción

--- src/shared/store/UserStore.ts
+++ src/shared/store/UserStore.ts
@@ -1,21 +1,20 @@
 import { create } from "zustand";
-import { ACCESS_TOKEN } from "../consts/ACCESS_TOKEN";
-import { IDecodifiedToken, IToken } from "../interfaces/IToken";
-import jwtDecode from "jwt-decode";
+// import { ACCESS_TOKEN } from "../consts/ACCESS_TOKEN";
+// import { IDecodifiedToken, IToken } from "../interfaces/IToken";
 import { persist, createJSONStorage } from "zustand/middleware";
-interface IUserInfo {
+/* interface IUserInfo {
   entity: number;
   setEntity: (value: IDecodifiedToken) => void;
-}
+} */
 
-const data = sessionStorage.getItem(ACCESS_TOKEN);
-const validateToken = () => {
+// const data = sessionStorage.getItem(ACCESS_TOKEN);
+/* const validateToken = () => {
   if (data !== null) {
     const dataParse: IToken = JSON.parse(data);
     const token: any = dataParse.data.token;
     return token;
   }
-};
+}; */
 // const testToken = validateToken();
 
 /* export const useUserInfoStore = create<IUserInfo>((set) => ({
